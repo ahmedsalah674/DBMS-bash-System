5a32fa046d68dc62a078a7c8d845d75bf919cf4d
Ahmed Shokry <ahmed.shokrg@gmail.com>
2022-01-05 21:51:06 +0200

    Delete from table v1

--- scripts/deletetb.sh
+++ scripts/deletetb.sh
@@ -4,62 +4,56 @@ projectPath=~/bash
 #database=$2
 database=ahmed
 
-if [[ $database ]]
-then
+read_col()
+{
     read -p "enter table name:" tableName
-    if [[ -f $projectPath/databases/$database/$tableName ]]
+    if [[ -f $1/$tableName ]]
     then
-    tName=$projectPath/databases/$database/$tableName
+        tName=$1/$tableName
         read -p "do you want to add a condition? [y/n]:" ch
         if [[ $ch = 'y' ]]
         then 
             read -p "enter the id you want to delete: " ID
-            #IFS=$'\n' read -d : -r -a lines <<< $projectPath/databases/$database/$tableName
-            size=`wc -l $tName | awk '{ print $1 }'`
-            #IFS=$' ' read -r -a lines <<< $size
-            #echo $size
-            #echo `tail -$(( $size-1 ))`
-            i=0
-            for number in `tail -$(( $size-1 )) $tName|cut -d: -f 2 | grep -nw $ID  | cut -d: -f 1` #| grep -nw ahmed | cut -d: -f 1`
-            do
-                echo "(($i))"
-                echo the id = $(($number-$i+1))
-                sed -i "$(($number-$i+1)) d" $tName
-                i=$(($i+1))
-            done
-
-            #echo "${lines[1]}"
-            # while()
-            # do 
-
-            # done
+            
         elif [[ $ch = 'n' ]]
         then
             echo "No"
         else
             echo "wrong entery"
         fi
+        size=`wc -l $tName | awk '{ print $1 }'`
     else
         echo "Table not found"
     fi
+    eval $2="'$tName'"
+    eval $3="'$size'"
+    eval $5="'$ID'"
+    eval $4="'$ch'"
+}
+
+delete_col()
+{
+    if [[ $3 = 'y' ]]
+    then
+        #base 
+        i=0
+        for number in `tail -$(( $2-1 )) $1|cut -d: -f 1 | grep -nw $4  | cut -d: -f 1` #| grep -nw ahmed | cut -d: -f 1`
+        do
+            #echo the id = $(($number-$i+1))
+            sed -i "$(($number-$i+1)) d" $1
+            i=$(($i+1))
+        done  
+    elif [[ $3 = 'n' ]]
+    then
+        echo "No"
+        sed -i "2,$2 d" $1
+    fi
+}
+
+if [[ $database ]]
+then
+    read_col "$projectPath/databases/$database" tname size choice ID
+    delete_col $tname $size $choice $ID
 else
     echo "Database not found"
 fi
-# delete_main()
-# {
-#     database=$2
-#     if [[ $database ]]
-#     then
-#         read -p "enter table name :" tableName
-#         if [[ -f $1/databases/$database/$tableName ]]
-#         then
-#             structureAndnumber "$1/databases/$database/$tableName"  columnsNumber
-#             insertValues "$1/databases/$database/$tableName" $columnsNumber
-#         else
-#             echo $1/databases/$database/$tableName    
-#             echo "<<table not found>>"
-#         fi
-#     else
-#         echo "<<no database connection>>"
-#     fi
-# }
